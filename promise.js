3f664208403b5d37132b2ea5e9abb82e1424086e
abdmaliksajid <abdmaliksajid@gmail.com>
2022-09-19 21:18:11 +0700

    Perbaikan fitur di file promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  //kita jalankan fungsi async awaitnya
+  let xxi = await promiseTheaterIXX();
+  let cgv = await promiseTheaterVGC();
+
+  //disini saya pakai filter, bisa juga pakai looping
+  return xxi.filter((i) => i.hasil === emosi).length + cgv.filter((i) => i.hasil === emosi).length;
+};
 
 module.exports = {
   promiseOutput,
